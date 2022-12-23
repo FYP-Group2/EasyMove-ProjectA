7f5e027724f79b88d65c593ccc8cc7c873fcb9df
Stephaniemei <80388954+Stephaniemei@users.noreply.github.com>
2022-12-24 02:55:33 +0800

    Add files via upload

---
+++ unable_withdraw.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
+import 'package:driver_integrated/account.dart';
+import 'package:driver_integrated/main.dart';
+
+class UnableWithdrawal extends StatelessWidget {
+  const UnableWithdrawal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(80.0),
+          child: AppBar(
+            title: Image.asset('assets/images/icon.png', height: 100),
+            centerTitle: true,
+            backgroundColor: Color(0xFFFFA600),
+          ),
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.only(left: 50, right: 50, bottom: 15),
+              padding: EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.orange, width: 3),
+                  borderRadius: BorderRadius.circular(25.0)),
+              child: Text(
+                "Unable to withdraw. Your wallet must have balance!",
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            GFButton(
+              color: Colors.orange,
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => AccountPage(title: "Account")));
+              },
+              text: "Done",
+              shape: GFButtonShape.pills,
+            ),
+          ],
+        )));
+  }
+}
--- wallet.dart
+++ wallet.dart
@@ -1,4 +1,5 @@
 import 'package:driver_integrated/merit.dart';
+import 'package:driver_integrated/unable_withdraw.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:getwidget/getwidget.dart';
@@ -9,21 +10,21 @@ import 'package:driver_integrated/withdrawal.dart';
 import 'package:driver_integrated/my_api_service.dart';
 
 final String url = "awcgroup.com.my";
-final Map<String, String> header = {'Content-Type': 'application/json; charset=UTF-8'};
+final Map<String, String> header = {
+  'Content-Type': 'application/json; charset=UTF-8'
+};
 bool withdraw = false;
 
-void makePostRequestWithdrawal(String url,String unencodedPath, Map<String,String> requestBody) async {
-  final response = await http.post(
-      Uri.http(url,unencodedPath),
-      body: requestBody
-  );
+void makePostRequestWithdrawal(
+    String url, String unencodedPath, Map<String, String> requestBody) async {
+  final response =
+      await http.post(Uri.http(url, unencodedPath), body: requestBody);
   // print(response.statusCode);
   // print(response.body);
   final data = json.decode(response.body);
   print(data);
   String msg = (data["message"]);
-  if (msg == "Request sent successfully.")
-  {
+  if (msg == "Request sent successfully.") {
     withdraw = true;
   }
 }
@@ -44,14 +45,17 @@ class walletPageState extends State<Wallet> {
     return Scaffold(
         appBar: AppBar(
           centerTitle: true,
-          title: Text("Wallet", style: const TextStyle(color: Colors.white),),
+          title: Text(
+            "Wallet",
+            style: const TextStyle(color: Colors.white),
+          ),
           backgroundColor: const Color.fromARGB(255, 255, 168, 0),
         ),
-        body:Column(
+        body: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             _commission(),
-            _requestbutton(context),
+            _requestcomsbutton(context),
             _merit(),
             _requestbutton(context),
             // _commissionlist(),
@@ -60,8 +64,9 @@ class walletPageState extends State<Wallet> {
   }
 
   //apiservice for wallet
-  Future<Map<String, dynamic>> initWallet() async{
-    final meritData = await MyApiService.getCommissionStatement(driver.id.toString());
+  Future<Map<String, dynamic>> initWallet() async {
+    final meritData =
+        await MyApiService.getCommissionStatement(driver.id.toString());
     widget.text = meritData;
     return meritData;
   }
@@ -88,19 +93,23 @@ class walletPageState extends State<Wallet> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text("COMMISSION + BONUS",
-            style: TextStyle(color: Colors.orange[400], fontSize: 18),),
+          Text(
+            "COMMISSION + BONUS",
+            style: TextStyle(color: Colors.orange[400], fontSize: 18),
+          ),
           FutureBuilder(
               future: initWallet(),
-              builder: (BuildContext context, AsyncSnapshot<Map<String, dynamic>> snapshot) {
-                if(snapshot.hasData){
-                  return Text("RM ${widget.text["commission_bonus"].toString()}",
-                    style: const TextStyle(fontSize: 30, color: Colors.black),);
-                }else{
+              builder: (BuildContext context,
+                  AsyncSnapshot<Map<String, dynamic>> snapshot) {
+                if (snapshot.hasData) {
+                  return Text(
+                    "RM ${widget.text["commission_bonus"].toString()}",
+                    style: const TextStyle(fontSize: 30, color: Colors.black),
+                  );
+                } else {
                   return const Text("Loading");
                 }
-              }
-          ),
+              }),
         ],
       ),
     );
@@ -109,43 +118,45 @@ class walletPageState extends State<Wallet> {
   //merit
   Widget _merit() {
     return FutureBuilder(
-      future: initWallet(),
-      builder: (BuildContext context, AsyncSnapshot<Map<String, dynamic>> snapshot) {
-        return Container(
-          width: double.maxFinite,
-          padding: EdgeInsets.all(10),
-          margin: EdgeInsets.only(left: 50, right: 50, top: 20),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(color: Colors.black, width: 0.1),
-            borderRadius: BorderRadius.circular(5.0),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 5,
-                blurRadius: 7,
-                offset: Offset(0, 3),
-              )
-            ],
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              if(snapshot.hasData)...[
-                Text("MERIT",
-                  style: TextStyle(color: Colors.orange[400], fontSize: 18),),
-                Text(
-                  "RM ${widget.text["withdrawable_merit"].toString()}",
-                  style: const TextStyle(fontSize: 30, color: Colors.black),
-                ),
-              ]else...[
-                const Text("Loading"),
-              ]
-            ],
-          ),
-        );
-      }
-    );
+        future: initWallet(),
+        builder: (BuildContext context,
+            AsyncSnapshot<Map<String, dynamic>> snapshot) {
+          return Container(
+            width: double.maxFinite,
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.only(left: 50, right: 50, top: 20),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.black, width: 0.1),
+              borderRadius: BorderRadius.circular(5.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3),
+                )
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (snapshot.hasData) ...[
+                  Text(
+                    "MERIT",
+                    style: TextStyle(color: Colors.orange[400], fontSize: 18),
+                  ),
+                  Text(
+                    "RM ${widget.text["withdrawable_merit"].toString()}",
+                    style: const TextStyle(fontSize: 30, color: Colors.black),
+                  ),
+                ] else ...[
+                  const Text("Loading"),
+                ]
+              ],
+            ),
+          );
+        });
   }
 
   //withdrawal button/function
@@ -153,23 +164,69 @@ class walletPageState extends State<Wallet> {
     String? user_id = driver.id.toString();
     final Map<String, String> body = {
       'uid': user_id,
-      'withdraw_mert': display_merit_value
+      'withdraw_merit': display_merit_value
     };
     final String unencodedPath = "/easymovenpick.com/api/request_withdraw.php";
-    return Padding (
-        padding: EdgeInsets.only(top:10,left:175),
+    return Padding(
+        padding: EdgeInsets.only(top: 10, left: 175),
         child: GFButton(
-        color: Colors.orange,
-        onPressed: () {
-          makePostRequestWithdrawal(url, unencodedPath, body);
-          if (withdraw == true) {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => Withdrawal()));
-          }
-          withdraw = false;
-        },
-        child: Text("Request Withdrawal"),
-        shape: GFButtonShape.pills,
-      ));
+          color: Colors.orange,
+          onPressed: () {
+            makePostRequestWithdrawal(url, unencodedPath, body);
+            if (withdraw == true) {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => Withdrawal()));
+            }
+            withdraw = false;
+          },
+          child: Text("Request Withdrawal"),
+          shape: GFButtonShape.pills,
+        ));
+  }
+
+  //withdrawal button/function
+  Widget _requestcomsbutton(context) {
+    String? user_id = driver.id.toString();
+    final Map<String, String> body = {
+      'uid': user_id,
+      'withdraw_merit': display_merit_value
+    };
+    final String unencodedPath = "/easymovenpick.com/api/request_withdraw.php";
+    return Padding(
+      padding: EdgeInsets.only(top: 10, left: 175),
+      child: FutureBuilder(
+          future: initWallet(),
+          builder: (BuildContext context,
+              AsyncSnapshot<Map<String, dynamic>> snapshot) {
+            return GFButton(
+              color: Colors.orange,
+              onPressed: () {
+                if (snapshot.hasData) {
+                  if (double.parse(widget.text["commission_bonus"]) > 5.99) {
+                    makePostRequestWithdrawal(url, unencodedPath, body);
+                    if (withdraw == true) {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Withdrawal()));
+                      display_merit_value = [
+                        (double.parse(widget.text["commission_bonus"]) + 5.00) -
+                            double.parse(widget.text["commission_bonus"])
+                      ].toString();
+                    }
+                    withdraw = false;
+                  } else {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => UnableWithdrawal()));
+                  }
+                }
+              },
+              child: Text("Request Withdrawal"),
+              shape: GFButtonShape.pills,
+            );
+          }),
+    );
   }
 }
